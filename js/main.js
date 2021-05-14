1ecd4171d25e85d7ad6a38c0d8fb067dcd4174eb
Etianne Contreras <etiannecb1994@gmail.com>
2021-05-14 18:19:47 -0400

    ver 2.1
    acumulativa

--- js/main.js
+++ js/main.js
@@ -42,5 +42,26 @@ $(document).ready(function(){
         $(target).fadeIn(600);
 
     });
+    function verificarPasswords(){
+        pass1 = document.getElementById('pass1');
+        pass2 = document.getElementById('pass2');
+
+        if (pass1.value != pass2.value) {
+            document.getElementById("error").classList.add("mostrar");
+
+            return false;
+        } else {
+
+            document.getElementById("error").classList.remove("mostrar");
+            document.getElementById("ok").classList.remove("ocultar");
+            document.getElementById("login").disable = true; 
+
+            setTimeout(function() {
+                location.reload();
+            }, 3000);
+
+            return true;
+        }
+    }
     
 });
